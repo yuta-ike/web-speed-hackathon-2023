2275a1c2c095e27c709e948fa90fa2d0063f66bb
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2023-05-08 02:22:19 +0900

    cssのSplitを有効 / cssTargetをデフォルトに

--- vite.config.ts
+++ vite.config.ts
@@ -24,8 +24,8 @@ export default defineConfig(async ({ mode }) => {
   return {
     build: {
       assetsInlineLimit: 20480,
-      cssCodeSplit: false,
-      cssTarget: 'es6',
+      // cssCodeSplit: false,
+      // cssTarget: 'es6',
       minify: true,
       rollupOptions: {
         output: {
