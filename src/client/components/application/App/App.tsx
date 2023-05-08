df11edf22d023870f0726bd2b7eecc497af715d9
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2023-05-08 23:28:42 +0900

    Modalをdynamic importに

--- src/client/components/application/App/App.tsx
+++ src/client/components/application/App/App.tsx
@@ -1,9 +1,12 @@
-import { SignInModal } from '../../modal/SignInModal';
-import { SignUpModal } from '../../modal/SignUpModal';
+import { lazy } from 'react';
+
 import { Providers } from '../Providers';
 import { Routes } from '../Routes';
 
 import type { FC } from 'react';
+
+const SignInModal = lazy(() => import('../../modal/SignInModal').then((res) => ({ default: res.SignInModal })));
+const SignUpModal = lazy(() => import('../../modal/SignUpModal').then((res) => ({ default: res.SignUpModal })));
 
 export const App: FC = () => (
   <Providers>
