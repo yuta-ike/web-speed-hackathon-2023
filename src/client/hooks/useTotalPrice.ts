ee453142b6e24a91ff88c4f8447ff9fdbaa0d380
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2023-05-08 21:41:22 +0900

    refactor

--- src/client/hooks/useTotalPrice.ts
+++ src/client/hooks/useTotalPrice.ts
@@ -1,30 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useMemo } from 'react';
 
 import { getActiveOffer } from '../utils/get_active_offer';
 
 import type { OrderFragmentResponse } from '../graphql/fragments';
 
 export function useTotalPrice(order: OrderFragmentResponse) {
-  const [totalPrice, setTotalPrice] = useState<number>(0);
-
-  useEffect(() => {
-    let timer = (function tick() {
-      return setImmediate(() => {
-        let total = 0;
-        for (const item of order.items) {
-          const offer = getActiveOffer(item.product.offers);
-          const price = offer?.price ?? item.product.price;
-          total += price * item.amount;
-        }
-        setTotalPrice(total);
-        timer = tick();
-      });
-    })();
-
-    return () => {
-      clearImmediate(timer);
-    };
-  }, [order]);
+  const totalPrice = useMemo(() => {
+    let total = 0;
+    for (const item of order.items) {
+      const offer = getActiveOffer(item.product.offers);
+      const price = offer?.price ?? item.product.price;
+      total += price * item.amount;
+    }
+    return total;
+  }, [order.items]);
 
   return { totalPrice };
 }
