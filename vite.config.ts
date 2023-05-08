28de778108e1b001dfd4284b2bb0e31e08fb44f8
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2023-05-09 04:53:10 +0900

    謎のvideo preloadを削除

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,3 @@
-import path from 'node:path';
-
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { ViteEjsPlugin } from 'vite-plugin-ejs';
@@ -7,21 +5,7 @@ import topLevelAwait from 'vite-plugin-top-level-await';
 import { visualizer } from 'rollup-plugin-visualizer';
 import viteCompression from 'vite-plugin-compression';
 
-import { getFileList } from './tools/get_file_list';
-
-const publicDir = path.resolve(__dirname, './public');
-const getPublicFileList = async (targetPath: string) => {
-  const filePaths = await getFileList(targetPath);
-  const publicFiles = filePaths
-    .map((filePath) => path.relative(publicDir, filePath))
-    .map((filePath) => path.join('/', filePath));
-
-  return publicFiles;
-};
-
 export default defineConfig(async ({ mode }) => {
-  const videos = await getPublicFileList(path.resolve(publicDir, 'videos'));
-
   return {
     build: {
       assetsInlineLimit: 20480,
@@ -50,8 +34,6 @@ export default defineConfig(async ({ mode }) => {
       ViteEjsPlugin({
         module: '/src/client/index.tsx',
         title: '買えるオーガニック',
-        // TODO: 気になる
-        videos,
       }),
     ],
   };
